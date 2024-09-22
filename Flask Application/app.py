aab14029343ae59a63157e96a050dd67326c516d
AKoudounis <andreaskoudounis@gmail.com>
2024-09-21 22:44:14 -0400

    az

--- Flask Application/app.py
+++ Flask Application/app.py
@@ -1,31 +1,10 @@
-import os
-from flask import Flask, request, jsonify
-import pandas as pd
-from azure.cosmos import CosmosClient
-from pyspark.sql import SparkSession
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-    
 
-# Initialize Spark session
-spark = SparkSession.builder \
-    .appName("FlaskApp") \
-    .getOrCreate()
-
-# Cosmos DB configuration
-cosmos_client = CosmosClient('https://cosmosdb424.documents.azure.com/', 'COSMOS_DB_KEY')
-database_name = 'TransactionsDB'
-container_name = 'Transactions'
-container = cosmos_client.get_database_client(database_name).get_container_client(container_name)
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run()  # Omit this in production
