7f57e57e989ce76c303489fc34397db0ecca4b2c
AKoudounis <andreaskoudounis@gmail.com>
2024-09-21 21:47:36 -0400

    change

--- Flask Application/app.py
+++ Flask Application/app.py
@@ -10,6 +10,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
+
 # Initialize Spark session
 spark = SparkSession.builder \
     .appName("FlaskApp") \
